3e496fa9b9fd68a328d9cd2d3f132333822cc8ec
fabi8bit <fabi8bit@gmail.com>
2023-05-19 10:28:49 +0000

    Initial commit

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,22 @@
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20,22)).toBe(42);
+        })
+        test("should return 50 for 20 + 30", () => {
+            expect(addition(20,30)).toBe(50);
+        })
+    });
+    describe("Subtraction function", () => {
+
+    });
+    describe("Multiply function", () => {
+
+    });
+    describe("Division function", () => {
+
+    });
+
+})
